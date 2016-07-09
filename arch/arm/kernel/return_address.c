004f608815bda91ad37ca50d81e6bc2cf02cbd00
tamirda <smstamir@gmail.com>
2016-07-09 12:06:34 +0300

    Compilation fixes, more GCC 5.3 patches, version bump, UBERTC implementation

--- arch/arm/kernel/return_address.c
+++ arch/arm/kernel/return_address.c
@@ -59,7 +59,7 @@ void *return_address(unsigned int level)
 #else /* if defined(CONFIG_FRAME_POINTER) && !defined(CONFIG_ARM_UNWIND) */
 
 #if defined(CONFIG_ARM_UNWIND)
-/* #warning "TODO: return_address should use unwind tables"
+/* #warning "TODO: return_address should use unwind tables" */
 #endif
 /*
 
@@ -67,7 +67,7 @@ void *return_address(unsigned int level)
 {
 	return NULL;
 }
-/*
+*/
 
 #endif /* if defined(CONFIG_FRAME_POINTER) && !defined(CONFIG_ARM_UNWIND) / else */
 
